7b2b63c29e5e68c643ae204c248ef85ac0429d2a
kidoc <kidoc@DESKTOP-6T3Q9GP>
2022-04-11 23:00:10 -0300

    Add project files.

---
+++ beecrowd soma simples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+ class program
+{
+    static void Main(string[] args)
+    {
+        int SOMA, A, B;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        SOMA = A + B;
+       
+        Console.WriteLine("SOMA = " + SOMA);
+        Console.ReadLine();
+
+
+    }
+
+}
